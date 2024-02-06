3d12073c21dc1b82aedbd28982d376501c17a5f1
Kago <kmotlhabane769@gmail.com>
2024-02-06 17:18:19 +0200

    Allow display of menu page skeleton

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-// TODO: Start with loading the initial part of page with event listeners to display other parts of the page
-
 import loadHomeSection from "./home.js";
 import loadContactSection from "./contact.js";
 import loadMenuSection from "./menu.js";
 
-// TODO: Add a display controller to control and manage all of this so we can render once through it and have internal logic controlled there but not as global variables
-
+// Display controller to control and manage all of the rendering logic (including initial single time rendering and other services) so there is no pollution of global variables
 let displayController = function () {
     let mainPageContainer = document.querySelector('div#content');
     let homeBtn = document.querySelector('button.home');
     let menuBtn = document.querySelector('button.menu');
     let contactBtn = document.querySelector('button.contact');
 
-    // It's as easy as removing and adding a container
     const initPage = () => mainPageContainer.append(loadHomeSection());
     initPage();
 
+    // It's as easy as removing and adding the current container at once with all the contents -- NOT a removal one by one
     const clearDisplay = () => {
         while (mainPageContainer.firstChild) {
             mainPageContainer.removeChild(mainPageContainer.lastChild);
--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,35 @@
 const loadMenuPage = function (){
-    let menuPage;
+    let menuDisplay = document.createElement('div');
+    menuDisplay.classList.add('menu');
 
-    return menuPage;
+    let itemsDisplay = document.createElement('div');
+    itemsDisplay.classList.add('wrapper');
+    let heading = document.createElement('h1');
+    // Prevent duplication by managing only/writing out the differences as unique instances
+    let menuItems = [{name: 'NAME 1', picUrl: 'URL', desc: 'Desc 1'}, {name: 'NAME 2', picUrl: 'URL', desc: 'Desc 2'}, {name: 'NAME 3',  picUrl: 'URL', desc: 'Desc 3'}];
+
+    heading.textContent = 'Chef\'s Highlights';
+    
+    menuItems.forEach(menuItem => {
+        let itemCard = document.createElement('div');
+        itemCard.classList.add('item', 'card');
+        let itemNameDisplay = document.createElement('h3');
+        let itemPicDisplay = document.createElement('div');
+        let itemDescDisplay = document.createElement('p');
+        
+        itemNameDisplay.textContent = menuItem.name;
+        itemPicDisplay.textContent = menuItem.picUrl;
+        itemDescDisplay.textContent = menuItem.desc;
+
+        itemCard.append(itemNameDisplay, itemPicDisplay, itemDescDisplay);
+
+        itemsDisplay.append(itemCard);
+    });
+
+    menuDisplay.append(heading);
+    menuDisplay.append(itemsDisplay);
+
+    return menuDisplay;
 };
 
 export default loadMenuPage;
