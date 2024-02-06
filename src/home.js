66400ae599c495f6141b1249ef771bcd95d79472
Kago <kmotlhabane769@gmail.com>
2024-02-06 13:51:10 +0200

    Load home page from function

---
+++ src/home.js
@@ -0,0 +1,33 @@
+/* 
+NOTE: Each page manages itself loading and itself loading only. 
+Global clear function can be elsewhere regardless of the specific contents here. 
+*/
+
+// Create and return element holding home page contents in this factory function
+const loadHomePage = function (){
+    // If I repeat this content section then it should always be static
+    // Append home page since I want a ready made element to append to content section, not add to content section on the fly
+    let homePage = document.createElement('div');
+    homePage.classList.add('home');
+
+    let about = document.createElement('section');
+
+    let heading = document.createElement('h2');
+    let pShortDesc = document.createElement('p');
+    let pHours = document.createElement('p');
+    let pMonFri = document.createElement('span');
+    let pSatSun = document.createElement('span');
+
+    pShortDesc.textContent = 'This is a description of our restaurant.'
+    pMonFri.textContent = "Mon-Fri: 9am - 9pm";
+    pSatSun.textContent = "Sat-Sun: 8am - 10pm";
+
+    pHours.append(pMonFri, pSatSun);
+    about.append(heading, pShortDesc, pHours);
+
+    homePage.append(about);
+
+    return homePage;
+};
+
+export default loadHomePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 // TODO: Start with loading the initial part of page with event listeners to display other parts of the page
+
+import loadHomePage from "./home.js";
+
+// TODO: Add a display controller to control and manage all of this so we can render once through it and have internal logic controlled there but not as global variables
+
+let contentDisplay = document.querySelector('div#content');
+
+contentDisplay.append(loadHomePage());
