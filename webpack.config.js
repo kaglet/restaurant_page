cc2dd282a19e53c42a08d673d0526fa4141133b8
Kago <kmotlhabane769@gmail.com>
2024-02-06 17:44:24 +0200

    Update webpack config file to reference moved src entry page

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/pages/index.js',
   mode: "development",
   devtool: 'inline-source-map',
   devServer: {
